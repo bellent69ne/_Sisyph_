e8098e6a796cf5c253058c0c31c1a68a3d506636
devmalic <devmalic@gmail.com>
2017-05-19 18:47:24 +0300

    Delete corrector.cpp

--- corrector.cpp
+++
@@ -1,6 +0,0 @@
-#include "corrector.hpp"
-
-void Corrector::usage() {
-	if(m_fullParams.size() == 0)
-		std::cout << "Usage: ./Sisyph whatToDo(-e, -d) encryption_key(-16, -32, or the actual_key) fullPath";
-}
